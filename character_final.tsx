5196ad6fda78f88826545d5793ebb8bbe40a7ea4
dannyg919 <66351626+dannyg919@users.noreply.github.com>
2021-11-22 01:47:01 -0500

    Resource Files pt 1

---
+++ character_final.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="character_final" tilewidth="16" tileheight="16" tilecount="12" columns="3">
+ <image source="../../Downloads/character_final.png" width="48" height="64"/>
+ <tile id="0"/>
+ <tile id="1"/>
+ <tile id="2"/>
+ <tile id="3"/>
+ <tile id="4"/>
+ <tile id="5"/>
+ <tile id="6"/>
+ <tile id="7"/>
+ <tile id="9"/>
+ <tile id="10"/>
+ <tile id="8"/>
+ <tile id="11"/>
+</tileset>
---
+++ maptiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="maptiles" tilewidth="16" tileheight="16" tilecount="176" columns="8">
+ <image source="poke_tileset.png" width="128" height="352"/>
+</tileset>
