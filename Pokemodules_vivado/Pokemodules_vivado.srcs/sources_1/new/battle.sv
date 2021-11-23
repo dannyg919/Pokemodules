31dfdf762fe3475958d935542231bec5d4f4bd4e
Azreen Zaman <zamanazreen@gmail.com>
2021-11-23 03:15:58 -0500

    inserting battle logic

--- Pokemodules_vivado/Pokemodules_vivado.srcs/sources_1/new/battle.sv
+++ Pokemodules_vivado/Pokemodules_vivado.srcs/sources_1/new/battle.sv
@@ -23,12 +23,12 @@ module blob
    #(parameter WIDTH = 64,            // default width: 64 pixels
                HEIGHT = 64,           // default height: 64 pixels
                COLOR = 12'hFFF)  // default color: white
-   (input wire [10:0] x_in,hcount_in,
+   (input wire [10:0] x_in,hcount_in,change_x,
     input wire [9:0] y_in,vcount_in,
     output logic [11:0] pixel_out);
 
    always_comb begin
-      if  ((hcount_in >= x_in && hcount_in < (x_in+WIDTH)) &&
+      if  ((hcount_in >= x_in && hcount_in < (x_in+WIDTH-change_x)) &&
             (vcount_in >= y_in && vcount_in < (y_in+HEIGHT)))
         pixel_out = COLOR;
       else 
@@ -36,11 +36,11 @@ module blob
    end
 endmodule
 
-module battle(input wire clk_in,input wire run,input wire [7:0] health_in,input wire [7:0] xp_in,
+module battle(input wire clk_in,input wire [7:0] health_in,input wire [7:0] xp_in,
 input wire rst_in,         // 1 to initialize module
    input wire left_in,          // move left
    input wire right_in,         // move right
-   
+   input wire select,
    input wire [10:0] hcount_in, // horizontal index of current pixel (0..1023)
    input wire [9:0]  vcount_in, // vertical index of current pixel (0..767)
    input wire hsync_in,         // XVGA horizontal sync signal (active low)
@@ -54,6 +54,7 @@ input wire rst_in,         // 1 to initialize module
 
     output logic battle, output logic start_timer, output logic [7:0] health_out, output logic [7:0] xp_out, output logic [3:0] state);
     logic turn;
+    logic run;
     logic [7:0] enemy_health;
     logic battle_start=1'b0;
     //logic [9:0] player_y;
@@ -64,43 +65,85 @@ input wire rst_in,         // 1 to initialize module
     logic [11:0] enemy_pixel;
     logic [10:0] enemy_x;
     
-    logic [10:0] enemy_health_x;
+    logic [10:0] enemy_health_change;
     logic [9:0] enemy_health_y;
     logic [11:0] enemy_health_pixel;
-    logic [10:0] player_health_x;
+    logic [10:0] player_health_change;
     logic [9:0] player_health_y;
     logic [11:0] player_health_pixel;
     logic [11:0] run_pixel,fight_pixel,menu_pixel,arrow_pixel;
-    parameter PLAYER_DAMAGE=10;
-    parameter ENEMY_DAMAGE=5;
+    logic [9:0] arrow_x;
+    parameter PLAYER_DAMAGE=20;
+    parameter ENEMY_DAMAGE=10;
     parameter XP_GAIN=10;
+    parameter RUN_POS=432+16+40+8;
+    parameter FIGHT_POS=432+8;
     
     always_ff @(posedge clk_in) begin
-    battle<=run?0:1;
-    if (!battle_start) begin
+    if (rst_in) begin
+        arrow_x<=FIGHT_POS;
         turn<=1'b1;
         enemy_health=7'd100;
         health_out<=health_in;
-        battle_start<=1;
-        
-    end
-    if (health_out>0 && enemy_health>0)begin 
-        if (battle) begin
-            if (turn) begin
-                health_out<=health_out-ENEMY_DAMAGE;
-            end else begin
-                enemy_health<=enemy_health-PLAYER_DAMAGE;
-            end
-            turn<=~turn;
-            end
-    end else begin
         battle<=0;
-        start_timer<=1;
-        state<=4'b010;
-        xp_out<=xp_in+XP_GAIN;
-    end
-    if (start_timer) begin
-         start_timer<=0; //make sure this is 0 if it is 1 after every clock cycle so that it doesnt keep restarting clock
+        enemy_health_change<=50;
+        player_health_change<=50;
+    end else begin
+    if (health_out>0 && enemy_health>0)begin
+        if (turn) begin
+            if (battle) begin
+                enemy_health<=enemy_health-PLAYER_DAMAGE;
+                enemy_health_change<=enemy_health_change-(PLAYER_DAMAGE/2);
+                turn<=~turn;
+            end else begin
+                if (left_in) begin
+                        arrow_x<=FIGHT_POS;
+                end
+                if (right_in)begin
+                        arrow_x<=RUN_POS;
+                end
+                if (select) begin
+                    if (arrow_x==FIGHT_POS)begin
+                        battle<=1;
+                    end else begin
+                        battle<=0;
+                        run<=1;
+                    end
+                end
+            end
+        end else begin
+            health_out<=health_out-ENEMY_DAMAGE;
+            player_health_change<=player_health_change-(ENEMY_DAMAGE/2);
+            turn<=~turn;
+        end
+   end else begin
+    run<=1;
+   end
+//    if (!battle_start) begin
+//        turn<=1'b1;
+//        enemy_health=7'd100;
+//        health_out<=health_in;
+//        battle_start<=1;
+        
+//    end
+//    if (health_out>0 && enemy_health>0)begin 
+//        if (battle) begin
+//            if (turn) begin
+//                health_out<=health_out-ENEMY_DAMAGE;
+//            end else begin
+//                enemy_health<=enemy_health-PLAYER_DAMAGE;
+//            end
+//            turn<=~turn;
+//            end
+//    end else begin
+//        battle<=0;
+//        start_timer<=1;
+//        state<=4'b010;
+//        xp_out<=xp_in+XP_GAIN;
+//    end
+//    if (start_timer) begin
+//         start_timer<=0; //make sure this is 0 if it is 1 after every clock cycle so that it doesnt keep restarting clock
+//    end
     end
      pixel_out <= {player_pixel[11:8] + enemy_pixel[11:8]+ player_health_pixel[11:8]+enemy_health_pixel[11:8]+run_pixel[11:8]+fight_pixel[11:8]+menu_pixel[11:8]+arrow_pixel[11:8],
                         player_pixel[7:4] + enemy_pixel[7:4] + player_health_pixel[7:4]+enemy_health_pixel[7:4]+run_pixel[7:4]+fight_pixel[7:4]+menu_pixel[7:4]+arrow_pixel[7:4],
@@ -118,12 +161,12 @@ input wire rst_in,         // 1 to initialize module
              
     blob #(.WIDTH(50),.HEIGHT(10),.COLOR(12'h0F0))   // green
      player_health(.x_in(432+14),.y_in(312+10),.hcount_in(hcount_in),.vcount_in(vcount_in),
-             .pixel_out(player_health_pixel));
+             .change_x(player_health_change),.pixel_out(player_health_pixel));
              
         
        blob #(.WIDTH(50),.HEIGHT(10),.COLOR(12'h0F0))   // green
      enemy_health_blob(.x_in(432+160-14-50),.y_in(312+144-40-14-10),.hcount_in(hcount_in),.vcount_in(vcount_in),
-             .pixel_out(enemy_health_pixel)); 
+             .change_x(enemy_health_change),.pixel_out(enemy_health_pixel)); 
         blob #(.WIDTH(40),.HEIGHT(8),.COLOR(12'hFF0))   // yellow
      fight(.x_in(432+16),.y_in(312+144-16-8),.hcount_in(hcount_in),.vcount_in(vcount_in),
              .pixel_out(fight_pixel));   
